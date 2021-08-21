bba91e85a7a013d5fff0e3c3c298622328c9da3a
luul negasi <37253956+Luultesfay@users.noreply.github.com>
2021-08-20 22:07:23 -0700

    Update addNumbers.js

--- addNumbers.js
+++ addNumbers.js
@@ -1,3 +1,8 @@
+//https://addition-domo.herokuapp.com/
+
+"use strict";
+
+
 let number1 = Math.trunc(Math.random() * 20) + 1;
 
 let number2 = Math.trunc(Math.random() * 20) + 1;
@@ -5,27 +10,45 @@ let number2 = Math.trunc(Math.random() * 20) + 1;
 let totalSum = number1 + number2;
 
 const numbers = document.querySelector('.RandNumber');
-document.querySelector('.n1').addEventListener('click', function () {
-  //console.log((numbers.textContent = number1));
+
+const Numb1=document.querySelector('.n1');
+const Numb2=document.querySelector('.nice');
+const total=document.querySelector('.tot');
+
+
+ Numb1.addEventListener('click', function () {
+  console.log((numbers.textContent = number1));
   //numbers.textContent = number1;
-  numbers.value = number1;
+numbers.value = number1;
+Numb1.style.backgroundColor='blue'
 });
 
-document.querySelector('.nice').addEventListener('click', function () {
+
+Numb2.addEventListener('click', function () {
   //console.log(document.querySelector('.RandNum').value);
   document.querySelector('.RandNum').value = number2;
+  Numb2.style.backgroundColor='red'
+  
 });
 
-document.querySelector('.tot').addEventListener('click', function () {
+total.addEventListener('click', function () {
   //console.log(document.querySelector('.totalNumber').value);
   document.querySelector('.totalNumber').value = totalSum;
+ total.style.backgroundColor='yellow'
 });
+
+
 
 document.querySelector('.reset').addEventListener('click', function () {
   numbers.value = 0;
+
+  Numb1.style.backgroundColor=''
+  Numb2.style.backgroundColor=''
+  total.style.backgroundColor=''
   document.querySelector('.RandNum').value = 0;
   document.querySelector('.totalNumber').value = 0;
   number1 = Math.trunc(Math.random() * 20) + 1;
   number2 = Math.trunc(Math.random() * 20) + 1;
   totalSum = number1 + number2;
 });
+
